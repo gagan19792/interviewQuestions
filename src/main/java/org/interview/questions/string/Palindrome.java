21a3df47091e93c7fe3c798c67a2e9fd97da10a0
Gagan <gagan.ubbey@gmail.com>
2025-05-10 17:38:28 -0700

    Solved

--- src/main/java/org/interview/questions/string/Palindrome.java
+++ src/main/java/org/interview/questions/string/Palindrome.java
@@ -26,7 +26,7 @@ package org.interview.questions.string;
 //s consists only of printable ASCII characters.
 
 
-public class    Palindrome {
+public class Palindrome {
     public static void main(String[] args) {
         String s1 = "A man, a plan, a canal: Panama";
         String s2 = "race a car";
